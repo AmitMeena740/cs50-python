fa6221cc851f336eb7d44a003c5db89455761528
AmitMeena740 <meenaamit74022@gmail.com>
2026-06-20 23:35:05 +0530

    Add electricity bill calculation script
    this is basically a electricity bill calculator which calculate electricity bill based on the rate per unit and total unit consume.

---
+++ week 1 practise extra/electricitybill.py
@@ -0,0 +1,7 @@
+x= float(input("How much unit electricity used ? "))
+# a unit equals to 1 kilowatt/hour or 1000watt/hour
+
+y= float(input("What is the rate of a unit ? "))
+z=x*y
+#electricity bill is calculated by multiplaying the rate of unit to the total unit used
+print(str(z) + "  Rupees Only ")
