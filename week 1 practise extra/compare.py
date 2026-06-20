21fcb3bc26b419f4493d5f6f070b5c221d56d964
AmitMeena740 <meenaamit74022@gmail.com>
2026-06-20 23:29:33 +0530

    Add compare.py to compare two integers

---
+++ week 1 practise extra/compare.py
@@ -0,0 +1,11 @@
+x = int(input(" what's x ? "))
+y= int(input("WHAT;S Y ?"))
+z = x+y
+if x<y :
+    print("x is less than y ")
+
+elif x>y:
+    print("x is greater than y")
+
+else :
+    print("x is equal to y ")
